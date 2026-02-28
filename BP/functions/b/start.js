d8c29f90e2c12ec56bcf41873311837c3623d640
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-02-28 15:30:33 +1100

    Update start.js

--- BP/functions/b/start.js
+++ BP/functions/b/start.js
@@ -3,13 +3,18 @@ execute if entity @a[tag=red] at @e[type=armor_stand,name="spawn.lobby"] run sum
 execute if entity @a[tag=blue] at @e[type=armor_stand,name="spawn.lobby"] run summon armor_stand spawn.blue ~~~
 execute if entity @a[tag=green] at @e[type=armor_stand,name="spawn.lobby"] run summon armor_stand spawn.green ~~~
 execute if entity @a[tag=yellow] at @e[type=armor_stand,name="spawn.lobby"] run summon armor_stand spawn.yellow ~~~
-## Spreading Stands (Single Relative)
+## Spreading Stands
 execute at @e[type=armor_stand,name="spawn.lobby"] run spreadplayers ~ ~ 50 @e[r=1,type=armor_stand]
-
-## Spreading Stands (Multipe)
 
 ## TP Players to Stands
 tp @a[tag=red] @e[type=armor_stand,name="spawn.red"] 
 tp @a[tag=blue] @e[type=armor_stand,name="spawn.blue"] 
 tp @a[tag=green] @e[type=armor_stand,name="spawn.green"] 
 tp @a[tag=yellow] @e[type=armor_stand,name="spawn.yellow"] 
+## Tag 'playing'
+tag @a[tag=red] add playin
+tag @a[tag=blue] add playing
+tag @a[tag=green] add playing
+tag @a[tag=yellow] add playing
+## Items
+give @a[tag=playing] iron_armor
