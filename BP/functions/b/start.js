6abef05c3d688193bf954ba19e4771519e915971
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-02-28 17:17:57 +1100

    Update start.js

--- BP/functions/b/start.js
+++ BP/functions/b/start.js
@@ -4,15 +4,14 @@ execute if entity @a[tag=blue] at @e[type=armor_stand,name="spawn.lobby"] run su
 execute if entity @a[tag=green] at @e[type=armor_stand,name="spawn.lobby"] run summon armor_stand spawn.green ~~~
 execute if entity @a[tag=yellow] at @e[type=armor_stand,name="spawn.lobby"] run summon armor_stand spawn.yellow ~~~
 ## Spreading Stands
-execute at @e[type=armor_stand,name="spawn.lobby"] run spreadplayers ~ ~ 50 @e[r=1,type=armor_stand]
-
+execute at @e[type=armor_stand,name="spawn.lobby"] run spreadplayers ~ ~ 50 @e[r=1,type=armor_stand,tag=!lock]
 ## TP Players to Stands
 tp @a[tag=red] @e[type=armor_stand,name="spawn.red"] 
 tp @a[tag=blue] @e[type=armor_stand,name="spawn.blue"] 
 tp @a[tag=green] @e[type=armor_stand,name="spawn.green"] 
 tp @a[tag=yellow] @e[type=armor_stand,name="spawn.yellow"] 
 ## Tag 'playing'
-tag @a[tag=red] add playin
+tag @a[tag=red] add playing
 tag @a[tag=blue] add playing
 tag @a[tag=green] add playing
 tag @a[tag=yellow] add playing
