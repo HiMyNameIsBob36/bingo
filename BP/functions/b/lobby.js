83b78626f5170d37d43d0ffd6daba1c261ac5c2c
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-02-28 13:46:31 +1100

    Update lobby.js

--- BP/functions/b/lobby.js
+++ BP/functions/b/lobby.js
@@ -5,21 +5,21 @@ execute as @a at @s if block ~~-1~ red_concrete run tag @s remove blue
 execute as @a at @s if block ~~-1~ red_concrete run tag @s remove green
 execute as @a at @s if block ~~-1~ red_concrete run tag @s remove yellow
 ## BLUE
-execute as @a at @s if block ~~-1~ red_concrete run tellraw @s[tag=!blue] {"rawtext":[{"text":"§lGAME §r§s>> §rJoined §1BLUE§r team."}]}
-execute as @a at @s if block ~~-1~ red_concrete run tag @s add blue
-execute as @a at @s if block ~~-1~ red_concrete run tag @s remove red
-execute as @a at @s if block ~~-1~ red_concrete run tag @s remove green
-execute as @a at @s if block ~~-1~ red_concrete run tag @s remove yellow
+execute as @a at @s if block ~~-1~ light_blue_concrete run tellraw @s[tag=!blue] {"rawtext":[{"text":"§lGAME §r§s>> §rJoined §bBLUE§r team."}]}
+execute as @a at @s if block ~~-1~ light_blue_concrete run tag @s add blue
+execute as @a at @s if block ~~-1~ light_blue_concrete run tag @s remove red
+execute as @a at @s if block ~~-1~ light_blue_concrete run tag @s remove green
+execute as @a at @s if block ~~-1~ light_blue_concrete run tag @s remove yellow
 ## GREEN
-execute as @a at @s if block ~~-1~ red_concrete run tellraw @s[tag=!green] {"rawtext":[{"text":"§lGAME §r§s>> §rJoined §1GREEN§r team."}]}
-execute as @a at @s if block ~~-1~ red_concrete run tag @s add green
-execute as @a at @s if block ~~-1~ red_concrete run tag @s remove red
-execute as @a at @s if block ~~-1~ red_concrete run tag @s remove blue
-execute as @a at @s if block ~~-1~ red_concrete run tag @s remove yellow
+execute as @a at @s if block ~~-1~ lime_concrete run tellraw @s[tag=!green] {"rawtext":[{"text":"§lGAME §r§s>> §rJoined §1GREEN§r team."}]}
+execute as @a at @s if block ~~-1~ lime_concrete run tag @s add green
+execute as @a at @s if block ~~-1~ lime_concrete run tag @s remove red
+execute as @a at @s if block ~~-1~ lime_concrete run tag @s remove blue
+execute as @a at @s if block ~~-1~ lime_concrete run tag @s remove yellow
 ## YELLOW
-execute as @a at @s if block ~~-1~ red_concrete run tellraw @s[tag=!yellow] {"rawtext":[{"text":"§lGAME §r§s>> §rJoined §eYELLOW§r team."}]}
-execute as @a at @s if block ~~-1~ red_concrete run tag @s add yellow
-execute as @a at @s if block ~~-1~ red_concrete run tag @s remove red
-execute as @a at @s if block ~~-1~ red_concrete run tag @s remove blue
-execute as @a at @s if block ~~-1~ red_concrete run tag @s remove green
+execute as @a at @s if block ~~-1~ yellow_concrete run tellraw @s[tag=!yellow] {"rawtext":[{"text":"§lGAME §r§s>> §rJoined §eYELLOW§r team."}]}
+execute as @a at @s if block ~~-1~ yellow_concrete run tag @s add yellow
+execute as @a at @s if block ~~-1~ yellow_concrete run tag @s remove red
+execute as @a at @s if block ~~-1~ yellow_concrete run tag @s remove blue
+execute as @a at @s if block ~~-1~ yellow_concrete run tag @s remove green
 
