3dbd63da44723ddbd1dd24e1b831a3f8eec33458
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-08 21:50:45 +1100

    Update start.js

--- BP/functions/rush/start.js
+++ BP/functions/rush/start.js
@@ -1,2 +1,10 @@
-summon armor_stand rush.spawn
-spreadplauers
+summon armor_stand rush.spawn ~~
+spreadplayers ~ ~ 50 100 @e[type=armor_stand,name=rush.spawn]
+tp @a[tag=runner] @e[name=rush.spawn]
+execute at @e[name=rush.spawn] run spreadplayers ~ ~ 2 5 @a[tag=hunter]
+playsound
+
+
+
+
+## execute if entity @a[tag=red]
