fc9f8b03360a56a5ade546a7826cd8781490900d
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-08 21:23:14 +1100

    Update check.js

--- BP/functions/rush/check.js
+++ BP/functions/rush/check.js
@@ -1,4 +1,4 @@
 ## Check >> Item
-execute if @a[tag=runner,hasitem={item=flint_and_steal}] run function rush/win
-execute if @a[tag=runner,hasitem={item=flint_and_steal}] run function rush/win
+execute if @a[tag=runner,hasitem={item=flint_and_steel}] run function rush/win
+execute if @a[tag=runner,hasitem={item=shears}] run function rush/win
 ## Check >> Death
