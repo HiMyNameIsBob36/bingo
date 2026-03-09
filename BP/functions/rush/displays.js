e04fd0e7f4fd02cfb8ffe2731804fc0f07ec0aa0
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-10 10:37:37 +1100

    Update displays.js

--- BP/functions/rush/displays.js
+++ BP/functions/rush/displays.js
@@ -1,1 +1,5 @@
 execute if score stage rushDB matches 1 if score item rushDB matches 1 run tititleraw @a title {"rawtext":[{"text":"§lAPPLE"}]}
+execute if score stage rushDB matches 1 if score item rushDB matches 2 run tititleraw @a title {"rawtext":[{"text":"§lDANDILION"}]}
+execute if score stage rushDB matches 1 if score item rushDB matches 3 run tititleraw @a title {"rawtext":[{"text":"§lSHEARS"}]}
+execute if score stage rushDB matches 1 if score item rushDB matches 4 run tititleraw @a title {"rawtext":[{"text":"§lFLINT & STEEL"}]}
+execute if score stage rushDB matches 1 if score item rushDB matches 5 run tititleraw @a title {"rawtext":[{"text":"§lSTONE STAIR"}]}
