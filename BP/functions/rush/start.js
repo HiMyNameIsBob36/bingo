cef63f38bf5948ebe9346aaf79bce0394f1d4a74
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-10 10:45:04 +1100

    Update start.js

--- BP/functions/rush/start.js
+++ BP/functions/rush/start.js
@@ -2,6 +2,7 @@ summon armor_stand rush.spawn ~~
 spreadplayers ~ ~ 50 100 @e[type=armor_stand,name=rush.spawn]
 tp @a[tag=runner] @e[name=rush.spawn]
 execute at @e[name=rush.spawn] run spreadplayers ~ ~ 2 5 @a[tag=hunter]
+imputpermission
 playsound
 
 scoreboard players random item rushDB 1 5
