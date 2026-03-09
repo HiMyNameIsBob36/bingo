3b1434be161aa6cfe1bb7fd6817b45c1173f579b
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-10 10:31:21 +1100

    Update displays.js

--- BP/functions/rush/displays.js
+++ BP/functions/rush/displays.js
@@ -1,1 +1,1 @@
-execute if score stage rushDB matches 1 if score item rushDB matches 1 run tiftitleraw @a title {"rawtext":[{"text":"§lAPPLE"}]}
+execute if score stage rushDB matches 1 if score item rushDB matches 1 run tititleraw @a title {"rawtext":[{"text":"§lAPPLE"}]}
