2ce666db42f906b6312fbfcfa6abff8672e272f9
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-10 10:44:03 +1100

    Update start.js

--- BP/functions/rush/start.js
+++ BP/functions/rush/start.js
@@ -4,4 +4,25 @@ tp @a[tag=runner] @e[name=rush.spawn]
 execute at @e[name=rush.spawn] run spreadplayers ~ ~ 2 5 @a[tag=hunter]
 playsound
 
-scoreboard players random item rushDB 1 10
+scoreboard players random item rushDB 1 5
+scoreboard players add item rushDB 1
+scoreboard players add item rushDB 1
+scoreboard players add item rushDB 1
+scoreboard players add item rushDB 1
+scoreboard players add item rushDB 1
+scoreboard players add item rushDB 1
+scoreboard players add item rushDB 1
+scoreboard players add item rushDB 1
+scoreboard players add item rushDB 1
+scoreboard players add item rushDB 1
+scoreboard players add item rushDB 0
+scoreboard players add item rushDB 1
+scoreboard players add item rushDB 0
+scoreboard players add item rushDB 1
+scoreboard players add item rushDB 0
+scoreboard players add item rushDB 0
+scoreboard players add item rushDB 1
+scoreboard players add item rushDB 0
+scoreboard players add item rushDB 0
+scoreboard players add item rushDB 0
+scoreboard players add item rushDB 1
