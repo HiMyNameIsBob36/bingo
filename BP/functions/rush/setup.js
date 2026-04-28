c3a40b6d4c76c76180ad5a76795ea3185fea74e0
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-28 23:11:19 +1000

    Delete BP/functions/rush/setup.js

--- BP/functions/rush/setup.js
+++
@@ -1,3 +0,0 @@
-tellraw @a {"rawtext":[{"text":"§l§sWelcome! §rI thank you for dowloading §sITEM RUSH§r. If you need help, feel free to reach out on our §9discord§r\nXDszHzgPBM"}]}
-scoreboard objectives add rushDB dummy
-tellraw @a {"rawtext":[{"text":"§l§aSuccess! The database has been setup! If you are still confused, you can type /function rush/help"}]}
