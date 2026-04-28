b59ec387bb7b7e40a9a0e42f4efb4b2aa663b153
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-29 00:06:36 +1000

    Update check.js

--- BP/functions/rush/check.js
+++ BP/functions/rush/check.js
@@ -5,3 +5,5 @@ execute if score item rushDB 1 if entity @a[tag=runner,hasitem={item=shears}] ru
 execute if score item rushDB 1 if entity @a[tag=runner,hasitem={item=flint_and_steel}] run scoreboard players set stage rushDB 3 
 execute if score item rushDB 1 if entity @a[tag=runner,hasitem={item=stone_stairs}] run scoreboard players set stage rushDB 3
 ## Check >> Death
+
+## Check >> Timer
