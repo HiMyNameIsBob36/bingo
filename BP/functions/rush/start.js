053ffeec5ad0c9459948162c7007bc705c5a0a28
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-28 23:10:59 +1000

    Update start.js

--- BP/functions/rush/start.js
+++ BP/functions/rush/start.js
@@ -1,7 +1,7 @@
 summon armor_stand rush.spawn ~~
-spreadplayers ~ ~ 50 100 @e[type=armor_stand,name=rush.spawn]
-tp @a[tag=runner] @e[name=rush.spawn]
-execute at @e[name=rush.spawn] run spreadplayers ~ ~ 2 5 @a[tag=hunter]
+spreadplayers ~ ~ 50 100 @e[type=armor_stand,name="rush.spawn"]
+tp @a[tag=runner] @e[name="rush.spawn"]
+execute at @e[name="rush.spawn"] run spreadplayers ~ ~ 2 5 @a[tag=hunter]
 imputpermission
 playsound
 
