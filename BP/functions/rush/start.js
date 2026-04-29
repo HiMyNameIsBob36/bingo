7442221747352d9c5745efa98352e022e5facdf6
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-29 18:03:13 +1000

    Update start.js

--- BP/functions/rush/start.js
+++ BP/functions/rush/start.js
@@ -1,12 +1,11 @@
 tag @a add ingame
 tag @r[tag=ingame] add runner
 tag @a[tag=!runner] add hunter
-spreadplayers ~ ~ 50 100 @a[tag=ingame]
+spreadplayers ~ ~ 1000 10000 @a[tag=runner]
+tp @a[tag=ingame] @a[tag=runner]
 summon armor_stand rush.spawn ~~~
-
 execute at @e[name="rush.spawn"] run spreadplayers ~ ~ 2 5 @a[tag=hunter]
 
-imputpermission
 playsound
 
 scoreboard players random item rushDB 1 5
