48f4f398237a54587edb8f01a9ab68f7fd5a2678
Sam <166279266+samlikesspace@users.noreply.github.com>
2026-04-30 08:29:17 +1000

    Update and rename display.js to global.js

--- BP/functions/i/global.js
+++ BP/functions/i/global.js
@@ -14,3 +14,13 @@ execute if score stage rushDB matches 2 if score item rushDB matches 5 run title
 
 
 execute if score item rushDB matches 6 run scoreboard players set item rushDB 0
+
+## Check >> Item
+execute if score item rushDB 1 if entity @a[tag=runner,hasitem={item=apple}] run scoreboard players set stage rushDB 3 
+execute if score item rushDB 2 if entity @a[tag=runner,hasitem={item=dandilion}] run scoreboard players set stage rushDB 3
+execute if score item rushDB 1 if entity @a[tag=runner,hasitem={item=shears}] run scoreboard players set stage rushDB 3 
+execute if score item rushDB 1 if entity @a[tag=runner,hasitem={item=flint_and_steel}] run scoreboard players set stage rushDB 3 
+execute if score item rushDB 1 if entity @a[tag=runner,hasitem={item=stone_stairs}] run scoreboard players set stage rushDB 3
+## Check >> Death
+
+## Check >> Timer
